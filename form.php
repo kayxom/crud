8951ee4b460d86a13ebab0830cdfaf47ae645f09
kayxom <143403330+kayxom@users.noreply.github.com>
2024-04-04 07:49:56 -0300

    Create form.php

---
+++ form.php
@@ -0,0 +1,18 @@
+<?php
+include ('conexao.php');
+$nome= $_POST['nome'];
+$email  = $_POST['email'];
+$telefone = $_POST['telefone'];
+$data_de_nacimento = $_POST['data_de_nacimento'];
+$cidade = $_POST['cidade'];
+$estado = $_POST['estado'];
+$endereço = $_POST['endereço'];
+$senha = $_POST['senha'];
+
+
+$resultado = mysql_query($conexao,"INSERT INTO login(nome,email,telefone,data_de_nacimento,cidade,estado, estado ,endereço,senha)VALUES('$)
+?>
+
+
+
+//erro no cod//
